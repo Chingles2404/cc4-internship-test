8ede189dcefbf2bb638a54aecf37a55a27d19892
CCH <chingles2404@gmail.com>
2023-02-21 11:40:09 +0800

    Created solution for Q3, removed try-except block in get_events(), added docstrings and other comments
    There exists rating titles in other languages, Q3 was written to accept only English rating titles as stated in the question.
    
    Changed try-except block to if statement to prevent over-filtering by only filtering out restaurants without events

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ import pandas as pd
 
 
 # ------------------------------
+# Function to create a general list of restaurants 
 
 
 
 def get_restaurants_list():
+    """ Add the multiple lists of restaurants without dummy restaurants and return the full list of actual restaurants """
     json_req = request.urlopen('https://raw.githubusercontent.com/Papagoat/brain-assessment/main/restaurant_data.json')
     data = json.load(json_req)
     restaurants = []
@@ -19,17 +21,28 @@ def get_restaurants_list():
                 restaurants.append(restaurant)
     return restaurants
 
-"""
-Extract following fields:
-- Restaurant Id
-- Restaurant Name
-- Country
-- City
-- User Rating Votes
-- User Aggregate Rating (in float)
-- Cuisines
-"""
+
+
+# ------------------------------
+# Part 1: Extract the following fields and store the data as restaurants.csv
+
+
+
 def get_restaurants_info(restaurant_list):
+    """
+    Extract the following fields:
+    - Restaurant Id
+    - Restaurant Name
+    - Country
+    - City
+    - User Rating Votes
+    - User Aggregate Rating (in float)
+    - Cuisines
+    And return it as a list of dictionaries
+
+    Keyword arguments:
+    restaurant_list -- the list of restaurants obtained from get_restaurants_list()
+    """
     result = []
     for restaurant in restaurant_list:
         temp = {}
@@ -44,6 +57,7 @@ def get_restaurants_info(restaurant_list):
     return result
 
 def country_codes():
+    """ Extract country codes from the Country-Code.xlsx Excel sheet and return the information as a dictionary """
     data = pd.read_excel("Country-Code.xlsx", engine="openpyxl")
     codes = {}
     for row in range(data.shape[0]):
@@ -51,10 +65,23 @@ def country_codes():
     return codes
     
 def modify_to_country(restaurants, codes):
+    """
+    Replaces the country code in each restaurant's information with the country name
+
+    Keyword arguments:
+    restaurants -- the list of dictionaries storing the restaurants' information
+    codes -- the dictionary of country codes obtained from country_codes()
+    """
     for restaurant in restaurants:
         restaurant["Country"] = codes[restaurant["Country"]]
 
 def create_restaurants(restaurants):
+    """
+    Creates the file restaurant.csv that displays the information of each restaurant
+
+    Keyword arguments:
+    restaurants -- the list of dictionaries storing the restaurants' information
+    """
     new_file = open("restaurant.csv", "w", encoding="utf-8")
     write_list = []
     temp = ""
@@ -66,25 +93,33 @@ def create_restaurants(restaurants):
         write_list.append(temp)
     new_file.writelines(write_list)
     new_file.close()
+    print("File restaurant.csv created successfully.")
 
-
-"""
-restaurant_list = get_restaurants_list()
-restaurants = get_restaurants_info(restaurant_list)
-codes = country_codes()
-modify_to_country(restaurants, codes)
-create_restaurants(restaurants)
-"""
 
 
 # ------------------------------
+# Part 2: Extract the list of restaurants that have past event in the month of April 2019 and store the data as restaurant_events.csv
 
 
 
 def get_events(restaurant_list):
+    """
+    Extract the following fields:
+    - Event Id
+    - Restaurant Id
+    - Restaurant Name
+    - Photo URL
+    - Event Title
+    - Event Start Date
+    - Event End Date
+    And return it as a list of dictionaries
+
+    Keyword arguments:
+    restaurant_list -- the list of restaurants obtained from get_restaurants_list()
+    """
     result = []
     for restaurant in restaurant_list:
-        try: 
+        if "zomato_events" in restaurant["restaurant"].keys():
             for event in restaurant["restaurant"]["zomato_events"]:
                 start_date = event["event"]["start_date"].split("-")
                 end_date = event["event"]["end_date"].split("-")
@@ -97,29 +132,108 @@ def get_events(restaurant_list):
                     temp["Event Id"] = event["event"]["event_id"]
                     temp["Restaurant Id"] = restaurant["restaurant"]["id"]
                     temp["Restaurant Name"] = restaurant["restaurant"]["name"]
-                    temp["Photo URL"] = restaurant["restaurant"]["photos_url"]
-                    # some titles have newlines which do not conform to csv formatting
+                    temp["Photo URL"] = event["event"]["photos"]
+                    # some titles have newlines which do not conform to CSV formatting
                     temp["Event Title"] = event["event"]["title"].strip()
                     temp["Event Start Date"] = event["event"]["start_date"]
                     temp["Event End Date"] = event["event"]["end_date"]
                     result.append(temp)
-        except:
-            KeyError
     return result
 
-def create_events(restaurants):
+def get_photo_url(events):
+    """
+    Replaces the list of photos in each event's information with a list of URLs
+
+    Keyword arguments:
+    events -- the list of dictionaries storing the events' information
+    """
+    for event in events:
+        temp = []
+        if len(event["Photo URL"]) == 0:
+            temp.append("NA")
+        else:
+            for photo in event["Photo URL"]:
+                temp.append(photo["photo"]["url"])
+        event["Photo URL"] = temp
+
+def create_events(events):
+    """
+    Creates the file restaurant_events.csv that displays the information of each restaurant event
+
+    Keyword arguments:
+    events -- the list of dictionaries storing the events' information
+    """
     new_file = open("restaurant_events.csv", "w", encoding="utf-8")
     write_list = []
     temp = ""
-    for key in restaurants[0].keys():
+    for key in events[0].keys():
         temp += key + ","
     write_list.append(temp[:-1] + "\n")
-    for restaurant in restaurants:
-        temp = f'{restaurant["Event Id"]},{restaurant["Restaurant Id"]},{restaurant["Restaurant Name"]},{restaurant["Photo URL"]},{restaurant["Event Title"]},{restaurant["Event Start Date"]},{restaurant["Event End Date"]}\n'
+    for event in events:
+        temp = f'{event["Event Id"]},{event["Restaurant Id"]},{event["Restaurant Name"]},{";".join(event["Photo URL"])},{event["Event Title"]},{event["Event Start Date"]},{event["Event End Date"]}\n'
         write_list.append(temp)
     new_file.writelines(write_list)
     new_file.close()
-    print("done")
+    print("File restaurant_events.csv created successfully.")
 
 
-create_events(get_events(get_restaurants_list()))
+
+# ------------------------------
+# Part 3: Determine the threshold for the different rating text based on aggregate rating
+
+
+
+def get_ratings(restaurant_list):
+    """
+    Find the range of aggregate ratings for each rating text and print the ranges out
+
+    Rating texts for the ranges to be found:
+    - Excellent
+    - Very Good
+    - Good
+    - Average
+    - Poor
+
+    Keyword arguments:
+    restaurant_list -- the list of restaurants obtained from get_restaurants_list()
+    """
+    dict_keys = ["Excellent", "Very Good", "Good", "Average", "Poor"]
+    ratings_dict = {}
+    for key in dict_keys:
+        ratings_dict[key] = {"lower": 5, "upper": 0}
+    
+    for restaurant in restaurant_list:
+        # ensure that the rating text is in title case
+        text = restaurant["restaurant"]["user_rating"]["rating_text"].title()
+        aggregate = float(restaurant["restaurant"]["user_rating"]["aggregate_rating"])
+        if text in ratings_dict.keys():
+            if aggregate < ratings_dict[text]["lower"]:
+                ratings_dict[text]["lower"] = aggregate
+            if aggregate > ratings_dict[text]["upper"]:
+                ratings_dict[text]["upper"] = aggregate
+    
+    print("The ranges of the aggregates for each rating text are")
+    for key in ratings_dict.keys():
+        print(f'{key}: {ratings_dict[key]["lower"]} - {ratings_dict[key]["upper"]}')
+
+
+# ------------------------------
+# Running of functions for the various parts
+
+
+
+restaurant_list = get_restaurants_list()
+
+# Part 1
+restaurants = get_restaurants_info(restaurant_list)
+codes = country_codes()
+modify_to_country(restaurants, codes)
+create_restaurants(restaurants)
+
+# Part 2
+events = get_events(restaurant_list)
+get_photo_url(events)
+create_events(events)
+
+# Part 3
+get_ratings(restaurant_list)
